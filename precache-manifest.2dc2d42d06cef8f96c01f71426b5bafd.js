3aa460e88683cb3a3bbd2b3bf5875e0943c8a732
Mohamed Galde <mgalde992@hotmail.com>
2020-04-25 23:44:14 +0300

    Updates

---
+++ precache-manifest.2dc2d42d06cef8f96c01f71426b5bafd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5ee3c5bd13b683087546c090a8bbcc",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "3009241ab5acf910a3e0",
+    "url": "/covid19/static/css/main.6f07c3ad.chunk.css"
+  },
+  {
+    "revision": "a8d5f599418ae1754db9",
+    "url": "/covid19/static/js/2.bdceaf34.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.bdceaf34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3009241ab5acf910a3e0",
+    "url": "/covid19/static/js/main.4749c55e.chunk.js"
+  },
+  {
+    "revision": "384fdf37c74f6a26a4c0",
+    "url": "/covid19/static/js/runtime-main.2993b3e3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
